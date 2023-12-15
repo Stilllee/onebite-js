f278daee0ebc10316c9bceba75076d201debf962
Stilllee <orodae@gmail.com>
2023-12-15 21:16:00 +0900

    [검색바 만들기]

---
+++ js/search.js
@@ -0,0 +1,17 @@
+const searchInput = document.getElementById("search-input");
+
+const showSearchResult = () => {
+  let searchWord = searchInput.value;
+  window.location.href = `https://www.google.com/search?q=${searchWord}`;
+  searchWord = "";
+};
+
+const enterKey = (e) => {
+  if (e.code === "Enter") {
+    showSearchResult();
+  }
+};
+
+searchInput.addEventListener("keypress", (e) => {
+  enterKey(e);
+});
