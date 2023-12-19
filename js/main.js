b35bb3d691602a597dd04ff1522a7981f8cf2b1e
Stilllee <orodae@gmail.com>
2023-12-19 16:04:31 +0900

    ✨ 투두리스트 삭제기능 구현

--- js/main.js
+++ js/main.js
@@ -21,11 +21,22 @@ const addTodoListItem = () => {
   setTodoListItem(todoText, date);
 };
 
+const deleteTodoListItem = (id) => {
+  const isDelete = window.confirm("정말 삭제하시겠습니까?");
+  if (isDelete) {
+    let todoList = JSON.parse(localStorage.getItem("todoList"));
+    let nowTodoList = todoList.filter((elm) => elm.date !== id);
+    localStorage.setItem("todoList", JSON.stringify(nowTodoList));
+    document.getElementById(`todo-item-${id}`).remove();
+  }
+};
+
 const setTodoListItem = (todoText, date) => {
   const todoList = document.getElementById("todo-list");
 
   const todoItem = document.createElement("div");
   todoItem.classList.add("todo-list-item");
+  todoItem.id = `todo-item-${date}`;
 
   const todo = document.createElement("div");
   todo.classList.add("todo");
@@ -39,6 +50,7 @@ const setTodoListItem = (todoText, date) => {
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "삭제";
   deleteBtn.classList.add("delete-btn");
+  deleteBtn.addEventListener("click", () => deleteTodoListItem(date));
 
   todoItem.appendChild(todo);
   todoItem.appendChild(completeBtn);
